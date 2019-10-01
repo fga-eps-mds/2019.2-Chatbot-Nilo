91db22bfaaac973c4cafbd00d832f8dc1821519b
Murilo Gomes <murilo.goomes@hotmail.com>
2019-09-30 23:11:28 -0300

    Correção de erro no arquivo procura_respostas.py

--- bot/actions/procura_respostas.py
+++ bot/actions/procura_respostas.py
@@ -5,10 +5,12 @@ def procura_resposta_por_topico(topico):
 		for line in file:
 			if line[1] == '.':
 				topico_atual = int(line[0])
+				continue
 			elif line[2] == '.':
 				topico_atual = int(line[1])*10 + int(line[0])
+				continue
 			if topico_atual == topico:
 				resposta.append(line)
 			elif topico_atual > topico:
 				break
-		return resposta
+	return resposta
