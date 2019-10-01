cdeda27da073e9fcf210274ac66646376fbfd7fd
Murilo Gomes <murilo.goomes@hotmail.com>
2019-10-01 00:29:48 -0300

    Adição da action_periodo_matricula

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -20,3 +20,13 @@ class ActionPreRequisitosEstagio(Action):
 				dispatcher.utter_message('{}'.format(line))
 		except ValueError:
 			dispatcher.utter_message(ValueError)
+
+class ActionPeriodoMatriculaEstagio(Action):
+	def name(self):
+		return 'action_periodo_matricula_estagio'
+	def run(self, dispatcher, tracker, domain):
+		try:
+			for line in procura_respostas.procura_resposta_por_topico(3):
+				dispatcher.utter_message('{}'.format(line))
+		except ValueError:
+			dispatcher.utter_message(ValueError)
--- coach/data/stories/actions.md
+++ coach/data/stories/actions.md
@@ -17,3 +17,13 @@
 ## path_action_pre-requisitos_estagio 2
 * action_pre-requisitos_estagio
 	- action_pre-requisitos_estagio
+
+## path_action_periodo_matricula_estagio 1
+* cumprimentar
+	- utter_cumprimentar
+* action_periodo_matricula_estagio
+	- action_periodo_matricula_estagio
+
+## path_action_periodo_matricula_estagio 2
+* action_periodo_matricula_estagio
+	- action_periodo_matricula_estagio
