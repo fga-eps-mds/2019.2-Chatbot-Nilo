7598d5393e799689c5af395d66918f0c7b543114
Murilo Gomes <murilo.goomes@hotmail.com>
2019-10-01 01:26:08 -0300

    Adição da action_carga-horaria_min_estagio

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -50,3 +50,13 @@ class ActionOrientadorEstagio(Action):
 				dispatcher.utter_message('{}'.format(line))
 		except ValueError:
 			dispatcher.utter_message(ValueError)
+
+class ActionCargaHorariaMinEstagio(Action):
+	def name(self):
+		return 'action_carga-horaria_min_estagio'
+	def run(self, dispatcher, tracker, domain):
+		try:
+			for line in procura_respostas.procura_resposta_por_topico(6):
+				dispatcher.utter_message('{}'.format(line))
+		except ValueError:
+			dispatcher.utter_message(ValueError)
--- coach/data/stories/actions.md
+++ coach/data/stories/actions.md
@@ -47,3 +47,13 @@
 ## path_action_orientador_estagio 2
 * action_orientador_estagio
 	- action_orientador_estagio
+
+## path_action_carga-horaria_min_estagio 1
+* cumprimentar
+	- utter_cumprimentar
+* action_carga-horaria_min_estagio
+	- action_carga-horaria_min_estagio
+
+## path_action_carga-horaria_min_estagio 2
+* action_carga-horaria_min_estagio
+	- action_carga-horaria_min_estagio
