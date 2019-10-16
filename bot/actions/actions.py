0d60a0cc66ffba0e53176ad50f1e1e5331a2043e
ThiagoGMF <thiago.guilherme20@hotmail.com>
2019-10-15 22:49:11 -0300

    Adicionando action que busca o nome de todos os coordenadores de estágio

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,5 +1,6 @@
 from rasa_core_sdk import Action
 import telegram
+from . import webscraping
 
 class ActionEnviarDocFaqEstagio(Action):
     def name(self):
@@ -11,3 +12,11 @@ class ActionEnviarDocFaqEstagio(Action):
         bot = telegram.Bot(token='ACCESS_TOKEN')
         url = 'https://aprender.ead.unb.br/pluginfile.php/688847/mod_resource/content/5/faq_estagio_supervisionado.pdf'
         bot.sendDocument(chat_id=tracker.sender_id,document=url)
+
+class ActionTodosOsCoordenadores(Action):
+	def name(self):
+		return "action_todos_os_coordenadores"
+
+	def run(self, dispatcher, tracker, domain):
+		dispatcher.utter_message("Em alguns instantes terei a lista de coordenadores e conseguirei informar Vossa Majestade...")
+		dispatcher.utter_message(webscraping.all_cordinators())
