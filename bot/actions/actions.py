0367085e8373b069f3549a5fd7e1e9fb27723d1f
Murilo Gomes <murilo.goomes@hotmail.com>
2019-11-05 13:30:14 -0300

    #59 Transformando os tabs em espaços
    Signed-off-by: Murilo Gomes <murilo.goomes@hotmail.com>
    Signed-off-by: Washington <juniorbispo515@gmail.com>
    
    Co-authored-by: Murilo Gomes <murilo.goomes@hotmail.com>
    Co-authored-by: Washington  <juniorbispo515@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -2,129 +2,130 @@ from rasa_core_sdk import Action
 import telegram
 from . import webscraping
 
-class ActionEnviarDocFaqEstagio(Action):
-	def name(self):
-		return "action_enviar_doc_faq_estagio"
 
-	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("Eu tenho muitas informações sobre estágio para você, Vossa Majestade!")
-		dispatcher.utter_message("Estou te enviando um documento com as dúvidas mais pertinentes que os alunos costumam ter")
-		bot = telegram.Bot(token='TELEGRAM_TOKEN')
-		url = 'https://aprender.ead.unb.br/pluginfile.php/688847/mod_resource/content/5/faq_estagio_supervisionado.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id,document=url)
+class ActionEnviarDocFaqEstagio(Action):
+    def name(self):
+        return "action_enviar_doc_faq_estagio"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Eu tenho muitas informações sobre estágio para você, Vossa Majestade!")
+        dispatcher.utter_message("Estou te enviando um documento com as dúvidas mais pertinentes que os alunos costumam ter")
+        bot = telegram.Bot(token='TELEGRAM_TOKEN')
+        url = 'https://aprender.ead.unb.br/pluginfile.php/688847/mod_resource/content/5/faq_estagio_supervisionado.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id,document=url)
 
 class ActionEnviarTCEPA(Action):
-	def name(self):
-		return "action_enviar_tce_pa"
+    def name(self):
+        return "action_enviar_tce_pa"
 
-	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("Eu tenho esses documentos de estágio supervisionado e não-obrigatório!")
-		dispatcher.utter_message("Estou te enviando o TCE e o PA")
-		bot = telegram.Bot(token='TELEGRAM_TOKEN')
-		url = 'https://fga.unb.br/articles/0002/2354/tce_para_estagio_obrigatorio.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id,document=url)
-		url2 = 'https://fga.unb.br/articles/0002/2356/tce_para_estagio_nao_obrigatorio.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id,document=url2)
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Eu tenho esses documentos de estágio supervisionado e não-obrigatório!")
+        dispatcher.utter_message("Estou te enviando o TCE e o PA")
+        bot = telegram.Bot(token='TELEGRAM_TOKEN')
+        url = 'https://fga.unb.br/articles/0002/2354/tce_para_estagio_obrigatorio.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id,document=url)
+        url2 = 'https://fga.unb.br/articles/0002/2356/tce_para_estagio_nao_obrigatorio.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id,document=url2)
 
 class ActionEnviarFichaSolicitacao(Action):
-	def name(self):
-		return "action_enviar_ficha_solicitacao"
+    def name(self):
+        return "action_enviar_ficha_solicitacao"
 
-	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("Eu tenho a ficha de solicitação de matrícula em estágio supervisionado!")
-		dispatcher.utter_message("Se você já estiver cadastrado na matéria, pode acessar uma ficha de preenchimento automático pelo moodle.")
-		dispatcher.utter_message("Estou te enviando a Ficha de Solicitação de Matrícula.")
-		bot = telegram.Bot(token='TELEGRAM_TOKEN')
-		url = 'https://aprender.ead.unb.br/pluginfile.php/610764/mod_resource/content/2/Anexo1_Ficha_Matr%C3%ADcula_Est%C3%A1gio_Supervisionado_FGA.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id,document=url)
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Eu tenho a ficha de solicitação de matrícula em estágio supervisionado!")
+        dispatcher.utter_message("Se você já estiver cadastrado na matéria, pode acessar uma ficha de preenchimento automático pelo moodle.")
+        dispatcher.utter_message("Estou te enviando a Ficha de Solicitação de Matrícula.")
+        bot = telegram.Bot(token='TELEGRAM_TOKEN')
+        url = 'https://aprender.ead.unb.br/pluginfile.php/610764/mod_resource/content/2/Anexo1_Ficha_Matr%C3%ADcula_Est%C3%A1gio_Supervisionado_FGA.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id,document=url)
 
 class ActionEnviarModeloDeTermoAditivo(Action):
-	def name(self):
-		return "action_enviar_modelo_de_termo_aditivo"
+    def name(self):
+        return "action_enviar_modelo_de_termo_aditivo"
 
-	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("Eu tenho o modelo de termo aditivo para te enviar!")
-		dispatcher.utter_message("Estou te enviando agora.")
-		bot = telegram.Bot(token='TELEGRAM_TOKEN')
-		url = 'https://fga.unb.br/articles/0002/2352/termo_aditivo_ao_tce.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id, document = url)
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Eu tenho o modelo de termo aditivo para te enviar!")
+        dispatcher.utter_message("Estou te enviando agora.")
+        bot = telegram.Bot(token='TELEGRAM_TOKEN')
+        url = 'https://fga.unb.br/articles/0002/2352/termo_aditivo_ao_tce.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id, document = url)
 
 class ActionEnviarModeloDeTermoRescisorio(Action):
-	def name(self):
-		return "action_enviar_termo_rescisorio"
+    def name(self):
+        return "action_enviar_termo_rescisorio"
 
-	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("Eu tenho o modelo de termo rescisório para te enviar!")
-		dispatcher.utter_message("Estou te enviando agora.")
-		bot = telegram.Bot(token='TELEGRAM_TOKEN')
-		url = 'https://fga.unb.br/articles/0002/2351/termo_de_rescisorio.pdf'
-		bot.sendDocument(chat_id=tracker.sender_id, document = url)
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Eu tenho o modelo de termo rescisório para te enviar!")
+        dispatcher.utter_message("Estou te enviando agora.")
+        bot = telegram.Bot(token='TELEGRAM_TOKEN')
+        url = 'https://fga.unb.br/articles/0002/2351/termo_de_rescisorio.pdf'
+        bot.sendDocument(chat_id=tracker.sender_id, document = url)
 
 class ActionTodosOsCoordenadores(Action):
-	def name(self):
-		return "action_todos_os_coordenadores"
+    def name(self):
+        return "action_todos_os_coordenadores"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("Então Vossa Majestade quer encontrar os Deuses...")
-		dispatcher.utter_message("Só um instante... Estou tentando lembrar o nome de todos...")
-		dispatcher.utter_message("Eng. Aeroespacial:")
-		dispatcher.utter_message(web.aeroespace_coordinator())
-		dispatcher.utter_message("Eng. Automotiva:")
-		dispatcher.utter_message(web.automotive_coordinator())
-		dispatcher.utter_message("Eng. Eletrônica:")
-		dispatcher.utter_message(web.eletronics_coordinator())
-		dispatcher.utter_message("Eng. de Software:")
-		dispatcher.utter_message(web.software_coordinator())
-		dispatcher.utter_message("Eng. de Energia:")
-		dispatcher.utter_message(web.energy_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("Então Vossa Majestade quer encontrar os Deuses...")
+        dispatcher.utter_message("Só um instante... Estou tentando lembrar o nome de todos...")
+        dispatcher.utter_message("Eng. Aeroespacial:")
+        dispatcher.utter_message(web.aeroespace_coordinator())
+        dispatcher.utter_message("Eng. Automotiva:")
+        dispatcher.utter_message(web.automotive_coordinator())
+        dispatcher.utter_message("Eng. Eletrônica:")
+        dispatcher.utter_message(web.eletronics_coordinator())
+        dispatcher.utter_message("Eng. de Software:")
+        dispatcher.utter_message(web.software_coordinator())
+        dispatcher.utter_message("Eng. de Energia:")
+        dispatcher.utter_message(web.energy_coordinator())
 
 class ActionCoordenadorDeSoftware(Action):
-	def name(self):
-		return "action_coordenador_de_software"
+    def name(self):
+        return "action_coordenador_de_software"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("Então Vossa Majestade finalmente se tornará um(a) grande desenvolvedor(a)")
-		dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
-		dispatcher.utter_message(web.software_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("Então Vossa Majestade finalmente se tornará um(a) grande desenvolvedor(a)")
+        dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
+        dispatcher.utter_message(web.software_coordinator())
 
 class ActionCoordenadorDeEnergia(Action):
-	def name(self):
-		return "action_coordenador_de_energia"
+    def name(self):
+        return "action_coordenador_de_energia"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("Tomara que você faça um sistema para captar toda a energia solar do deserto do Egito!")
-		dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
-		dispatcher.utter_message(web.energy_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("Tomara que você faça um sistema para captar toda a energia solar do deserto do Egito!")
+        dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
+        dispatcher.utter_message(web.energy_coordinator())
 
 class ActionCoordenadorDeAeroespacial(Action):
-	def name(self):
-		return "action_coordenador_de_aeroespacial"
+    def name(self):
+        return "action_coordenador_de_aeroespacial"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("Tomara que Vossa Majestade construa um avião para que possa visitar o Egito...")
-		dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
-		dispatcher.utter_message(web.aeroespace_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("Tomara que Vossa Majestade construa um avião para que possa visitar o Egito...")
+        dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
+        dispatcher.utter_message(web.aeroespace_coordinator())
 
 class ActionCoordenadorDeEletronica(Action):
-	def name(self):
-		return "action_coordenador_de_eletronica"
+    def name(self):
+        return "action_coordenador_de_eletronica"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("UAU espero que Vossa Majestade possa criar várias coisas incriveis...")
-		dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajuda")
-		dispatcher.utter_message(web.eletronics_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("UAU espero que Vossa Majestade possa criar várias coisas incriveis...")
+        dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajuda")
+        dispatcher.utter_message(web.eletronics_coordinator())
 
 class ActionCoordenadorDeAutomotiva(Action):
-	def name(self):
-		return "action_coordenador_de_automotiva"
+    def name(self):
+        return "action_coordenador_de_automotiva"
 
-	def run(self, dispatcher, tracker, domain):
-		web = webscraping.WebScrapingIntegration()
-		dispatcher.utter_message("Então Vossa Majestade finalmente está pronto para construir os carros que desbravarão o deserto...")
-		dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
-		dispatcher.utter_message(web.automotive_coordinator())
+    def run(self, dispatcher, tracker, domain):
+        web = webscraping.WebScrapingIntegration()
+        dispatcher.utter_message("Então Vossa Majestade finalmente está pronto para construir os carros que desbravarão o deserto...")
+        dispatcher.utter_message("Só um instante... Estou buscando o guia certo para lhe ajudar")
+        dispatcher.utter_message(web.automotive_coordinator())
--- bot/actions/webscraping.py
+++ bot/actions/webscraping.py
@@ -1,134 +1,135 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class WebScrapingIntegration:
-	def __init__(self):
-		self.url = 'https://fga.unb.br/coordenacao'
-		self.request = requests.get(self.url)
-		self.soup = BeautifulSoup(self.request.text, 'html.parser')
-		self.coordinators = self.soup.find_all('div', class_='article-body article-body-text-article')
+    def __init__(self):
+        self.url = 'https://fga.unb.br/coordenacao'
+        self.request = requests.get(self.url)
+        self.soup = BeautifulSoup(self.request.text, 'html.parser')
+        self.coordinators = self.soup.find_all('div', class_='article-body article-body-text-article')
 
-	def software_coordinator(self):
-		names = ''
-		find_coordinator = True
-		for coord_td in self.coordinators:
-			list_tds = coord_td.find_all('td')
-			span_counter = int(0)
-			for coord_list in list_tds:
-				html_next_element = coord_list.next_element
-				if html_next_element.find('span') != -1:
-					span_counter+=1
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Software') == -1:
-					find_coordinator = False
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Software') != -1:
-					find_coordinator = True
-					continue
-				elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
-					names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
-					names+='\n'
-				elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
-					names+= coord_list.next_element
-					names+='\n'
-		return names
+    def software_coordinator(self):
+        names = ''
+        find_coordinator = True
+        for coord_td in self.coordinators:
+            list_tds = coord_td.find_all('td')
+            span_counter = int(0)
+            for coord_list in list_tds:
+                html_next_element = coord_list.next_element
+                if html_next_element.find('span') != -1:
+                    span_counter+=1
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Software') == -1:
+                    find_coordinator = False
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Software') != -1:
+                    find_coordinator = True
+                    continue
+                elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
+                    names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
+                    names+='\n'
+                elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
+                    names+= coord_list.next_element
+                    names+='\n'
+        return names
 
-	def energy_coordinator(self):
-		names = ''
-		find_coordinator = True
-		for coord_td in self.coordinators:
-			list_tds = coord_td.find_all('td')
-			span_counter = int(0)
-			for coord_list in list_tds:
-				html_next_element = coord_list.next_element
-				if html_next_element.find('span') != -1:
-					span_counter += 1
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Energia') == -1:
-					find_coordinator = False
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Energia') != -1:
-					find_coordinator = True
-					continue
-				elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
-					names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
-					names += '\n'
-				elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
-					names += coord_list.next_element
-					names += '\n'
-		return names
+    def energy_coordinator(self):
+        names = ''
+        find_coordinator = True
+        for coord_td in self.coordinators:
+            list_tds = coord_td.find_all('td')
+            span_counter = int(0)
+            for coord_list in list_tds:
+                html_next_element = coord_list.next_element
+                if html_next_element.find('span') != -1:
+                    span_counter += 1
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Energia') == -1:
+                    find_coordinator = False
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Energia') != -1:
+                    find_coordinator = True
+                    continue
+                elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
+                    names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
+                    names += '\n'
+                elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
+                    names += coord_list.next_element
+                    names += '\n'
+        return names
 
-	def aeroespace_coordinator(self):
-		names = ''
-		find_coordinator = True
-		for coord_td in self.coordinators:
-			list_tds = coord_td.find_all('td')
-			span_counter = int(0)
-			for coord_list in list_tds:
-				html_next_element = coord_list.next_element
-				if html_next_element.find('span') != -1:
-					span_counter += 1
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Aeroespacial') == -1:
-					find_coordinator = False
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Aeroespacial') != -1:
-					find_coordinator = True
-					continue
-				elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
-					names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
-					names += '\n'
-				elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
-					names += coord_list.next_element
-					names += '\n'
-		return names
+    def aeroespace_coordinator(self):
+        names = ''
+        find_coordinator = True
+        for coord_td in self.coordinators:
+            list_tds = coord_td.find_all('td')
+            span_counter = int(0)
+            for coord_list in list_tds:
+                html_next_element = coord_list.next_element
+                if html_next_element.find('span') != -1:
+                    span_counter += 1
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Aeroespacial') == -1:
+                    find_coordinator = False
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Aeroespacial') != -1:
+                    find_coordinator = True
+                    continue
+                elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
+                    names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
+                    names += '\n'
+                elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
+                    names += coord_list.next_element
+                    names += '\n'
+        return names
 
-	def automotive_coordinator(self):
-		names = ''
-		find_coordinator = True
-		for coord_td in self.coordinators:
-			list_tds = coord_td.find_all('td')
-			span_counter = int(0)
-			for coord_list in list_tds:
-				html_next_element = coord_list.next_element
-				if html_next_element.find('span') != -1:
-					span_counter += 1
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Automotiva') == -1:
-					find_coordinator = False
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Automotiva') != -1:
-					find_coordinator = True
-					continue
-				elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
-					names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
-					names += '\n'
-				elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
-					names += coord_list.next_element
-					names += '\n'
-		return names
+    def automotive_coordinator(self):
+        names = ''
+        find_coordinator = True
+        for coord_td in self.coordinators:
+            list_tds = coord_td.find_all('td')
+            span_counter = int(0)
+            for coord_list in list_tds:
+                html_next_element = coord_list.next_element
+                if html_next_element.find('span') != -1:
+                    span_counter += 1
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Automotiva') == -1:
+                    find_coordinator = False
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Automotiva') != -1:
+                    find_coordinator = True
+                    continue
+                elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
+                    names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
+                    names += '\n'
+                elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
+                    names += coord_list.next_element
+                    names += '\n'
+        return names
 
-	def eletronics_coordinator(self):
-		names = ''
-		find_coordinator = True
-		for coord_td in self.coordinators:
-			list_tds = coord_td.find_all('td')
-			span_counter = int(0)
-			for coord_list in list_tds:
-				html_next_element = coord_list.next_element
-				if html_next_element.find('span') != -1:
-					span_counter += 1
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Eletrônica') == -1:
-					find_coordinator = False
-					continue
-				elif html_next_element.find('Eng.') != -1 and html_next_element.find('Eletrônica') != -1:
-					find_coordinator = True
-					continue
-				elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
-					names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
-					names += '\n'
-				elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
-					names += coord_list.next_element
-					names += '\n'
-		return names
+    def eletronics_coordinator(self):
+        names = ''
+        find_coordinator = True
+        for coord_td in self.coordinators:
+            list_tds = coord_td.find_all('td')
+            span_counter = int(0)
+            for coord_list in list_tds:
+                html_next_element = coord_list.next_element
+                if html_next_element.find('span') != -1:
+                    span_counter += 1
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Eletrônica') == -1:
+                    find_coordinator = False
+                    continue
+                elif html_next_element.find('Eng.') != -1 and html_next_element.find('Eletrônica') != -1:
+                    find_coordinator = True
+                    continue
+                elif coord_list.next_element.next_element.name == 'a' and span_counter > 4 and find_coordinator:
+                    names = names + 'Prof(a). ' + coord_list.next_element.next_element.next_element + ' ' + coord_list.next_element.next_element.next_element.next_element
+                    names += '\n'
+                elif span_counter > 4 and find_coordinator and coord_list.next_element != '\xa0':
+                    names += coord_list.next_element
+                    names += '\n'
+        return names
